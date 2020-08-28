9c9f51b39bcc4e4f597ea20eee59439463040b49
Stephanie Galvan <55038687+stephypy@users.noreply.github.com>
2020-08-28 14:28:56 -0600

    (to be deleted) include list of general tasks to complete assignment

--- wordCount.py
+++ wordCount.py
@@ -3,3 +3,11 @@
 # Assignment 0: Python Intro
 # Description: Given a text file, outputs a file with the total number of times each word in the given file
 # appears (case insensitive) in alphabetical order
+
+# TODO: @stephypy
+# 1. Verify command line args are  valid (only 2 args, they're both text files)
+# 2. Verify the input file exist; if output file doesnt exist then create it
+# 3. Set a re to only read words (no case sensitive, ignore punctuation)
+# 4. Read input file and save each word in dict with total number of appearances
+# 5. Sort dict and save contents on output file (word <space> num per line)
+# 6. Final testing and complete submission!
