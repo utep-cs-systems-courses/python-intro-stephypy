1f9f350354244fd79687f4bd4ca4f0233ec5a1f0
Stephanie <55038687+stephypy@users.noreply.github.com>
2020-08-28 14:17:37 -0600

    Added a header to wordCount.py which will contain the assignment progress

---
+++ wordCount.py
@@ -0,0 +1,5 @@
+# Author: Stephanie Galvan
+# Class: Theory of Operating Systems
+# Assignment 0: Python Intro
+# Description: Given a text file, outputs a file with the total number of times each word in the given file
+# appears (case insensitive) in alphabetical order
