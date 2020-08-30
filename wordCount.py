382af7e7f777d83c83f33dbd18d937a215d7dedd
Stephanie Galvan <55038687+stephypy@users.noreply.github.com>
2020-08-30 01:11:09 -0600

    added python version on header

--- wordCount.py
+++ wordCount.py
@@ -1,6 +1,7 @@
 # Author: Stephanie Galvan
 # Class: Theory of Operating Systems
 # Assignment 0: Python Intro
+# Python version: 3.8.5
 # Description: Given a text file, outputs a file with the total number of times each word in the given file
 # appears (case insensitive) in alphabetical order
 
